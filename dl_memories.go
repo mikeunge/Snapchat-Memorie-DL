ddde4630ad5ecbe985aab019eae49ee5e3cb058c
mikeunge <mikeunge@protonmail.com>
2022-02-10 13:43:32 +0100

    chg: made the downloads to go-routines so we can download multiple images/videos at once

--- dl_memories.go
+++ dl_memories.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
 
 type DLObject struct {
@@ -22,59 +24,74 @@ type Media struct {
 
 var logger log.Logger
 
-func save(id int, obj DLObject) error {
-	id = id + 1
+func getMeta(id int, meta *DLObject) (string, string, error) {
+	var baseDir string
+	var extenstion string
+
+	if meta.Media == "Image" {
+		baseDir = "images/"
+		extenstion = ".jpg"
+	} else if meta.Media == "Video" {
+		baseDir = "videos/"
+		extenstion = ".mp4"
+	} else {
+		e := fmt.Errorf("[WARN] %d - media type unknown, skipping\n", id)
+		logger.Println(e)
+		return "", "", e
+	}
+
+	return baseDir, extenstion, nil
+}
+
+func save(id int, obj *DLObject) error {
 	fmt.Fprintf(os.Stdout, "[INFO] %d - starting download\n", id)
 	response, e := http.Post(obj.Link, "application/x-www-form-urlencoded", nil)
 	if e != nil {
 		err := fmt.Errorf("[ERRO] %d - response error occured for element\n", id)
-		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
+		fmt.Fprintf(os.Stderr, "[ERRO] %d - response error occured for element\n", id)
+		// logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
 		return err
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		err := fmt.Errorf("[ERRO] %d - response status code is not correct for element\n", id)
+		e := fmt.Errorf("[ERRO] %d - response status code is not correct for element\n", id)
 		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
-		return err
+		// fmt.Fprintf(os.Stderr, "%+v", e)
+		return e
 	}
 
-	var url = fmt.Sprintf("%+v", response.Body)
-	fmt.Fprintf(os.Stdout, "[INFO] %d - returned url: %s\n", id, url)
-	res, e := http.Post(url, "application/x-www-form-urlencoded", nil)
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(response.Body)
+	url := buf.String()
+	res, e := http.Get(url)
 	if e != nil {
 		err := fmt.Errorf("[ERRO] %d - response error occured for element\n", id)
 		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
+		// fmt.Fprintf(os.Stderr, "%+v", err)
 		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		err := fmt.Errorf("[ERRO] %d - response status code is not correct for element\n", id)
+		e := fmt.Errorf("[ERRO] %d - response status code is not correct for element\n", id)
 		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
-		return err
+		// fmt.Fprintf(os.Stderr, "%+v", e)
+		return e
 	}
 	fmt.Fprintf(os.Stdout, "[INFO] %d - download finished\n", id)
 
-	var baseDir string
-	var extenstion string
-	if obj.Media == "Image" {
-		baseDir = "images/"
-		extenstion = ".jpg"
-	} else if obj.Media == "Video" {
-		baseDir = "videos/"
-		extenstion = ".mp4"
-	} else {
-		err := fmt.Errorf("[WARN] %d - media type unknown, skipping\n", id)
-		logger.Println(err)
-		return err
+	baseDir, extension, e := getMeta(id, obj)
+	if e != nil {
+		return e
 	}
 
-	var dir = fmt.Sprintf("%s%s - %d.%s", baseDir, obj.Date, id, extenstion)
+	var dir = fmt.Sprintf("%s%s - %d.%s", baseDir, obj.Date, id, extension)
 	file, e := os.Create(dir)
 	if e != nil {
 		err := fmt.Errorf("[ERRO] %d - could not create file: %s\n", id, dir)
 		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
+		// fmt.Fprint(os.Stderr, "%+v", err)
 		return err
 	}
 	defer file.Close()
@@ -85,10 +102,22 @@ func save(id int, obj DLObject) error {
 	if e != nil {
 		err := fmt.Errorf("[ERRO] %d - could not write to file: %s\n", id, dir)
 		logger.Println(fmt.Sprintf("[ERRO] %d - %+v", id, e))
+		// fmt.Fprint(os.Stderr, "%+v", err)
 		return err
 	}
 	fmt.Fprintf(os.Stdout, "[INFO] %d - %s successfully saved: %s\n", id, obj.Media, dir)
 	return nil
+}
+
+func worker(wChan chan DLObject, idChan chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for obj := range wChan {
+		e := save(<-idChan, &obj)
+		if e != nil {
+			fmt.Println(e)
+		}
+	}
 }
 
 func init() {
@@ -101,6 +130,7 @@ func init() {
 }
 
 func main() {
+	const workers = 100
 	var jsonFilePath = "json/memories_history.json"
 	jsonFile, e := os.Open(jsonFilePath)
 	if e != nil {
@@ -113,9 +143,22 @@ func main() {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &media)
 
-	fmt.Fprintf(os.Stdout, "[INFO] data read... starting to download %d elements\n", len(media.SavedMedia))
-	for i := 0; i < 5; i++ {
-		e = save(i, media.SavedMedia[i])
-		fmt.Println(e)
+	c := make(chan DLObject)
+	iC := make(chan int)
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go worker(c, iC, wg)
 	}
+
+	fmt.Fprintf(os.Stdout, "[INFO] data read... starting to download %d elements\n", len(media.SavedMedia))
+	for id, obj := range media.SavedMedia {
+		c <- obj
+		iC <- id
+	}
+
+	close(c)
+	wg.Wait()
+
 }
